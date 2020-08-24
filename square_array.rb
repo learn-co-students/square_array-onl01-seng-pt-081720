bd1f19d1b554409f2569ca5256be18241cafd1c9
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 18:15:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    number = number**2
+    new_array << number
+  end
+  new_array
 end
+
+test_array = [10,12,20]
+new_collect_array = []
+new_collect_array = test_array.collect { |x| x**2 }
+binding.pry
