a36463b2e9f85425d9df91d37a199a457964cba8
Dominic Barajas <dominicbarajas548@gmail.com>
2020-08-27 01:03:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,16 @@
 
+#def square_array(array)
+  #array_squared =[]
+  #array.each do |numbers|
+    #square_array
+#  end
+#    array_squared
+#end
+
 def square_array(array)
-squared = []
-array.each { |element| squared << element ** 2 }
-squared
+  squared_numbers = []
+  array.each do |num|
+    squared_numbers << num ** 2
+end
+  squared_numbers
 end
