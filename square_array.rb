eb4876ceda99a9f70b9af17a74038641bb023e19
Justice Payne <spikepayne@yahoo.com>
2020-08-27 03:09:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+# define square array method
+# define variables to the numbers array
+# returns a new array
+# use iterator .each
+
+require 'pry'
+
+
 def square_array(array)
-  # your code here
+  numbers = [1,2,3]
+  empty_array = []
+  array.each do |numbers|
+  empty_array << numbers ** 2
+  end
+  empty_array
 end
